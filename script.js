4b10fb71274a84dfe95a11712de1d22ae6a51133
Matty <matty@Mattys.local>
2018-08-02 12:42:12 +0100

    added functionality to nav-items

---
+++ script.js
@@ -0,0 +1,8 @@
+const navItems = document.querySelector('.navigation__list');
+const checkbox = document.getElementById('nav-toggle');
+
+navItems.addEventListener('click', closeNav);
+
+function closeNav() {
+  checkbox.checked = false;
+}
